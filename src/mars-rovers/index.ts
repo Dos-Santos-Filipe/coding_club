120328c08362eb50137ad4aac1058ab22172a459
Filipe Dos Santos <filipesantos.psic@gmail.com>
2024-10-21 18:11:00 -0300

    mars-rovers: wip/adds part of moveRover function

--- src/mars-rovers/index.ts
+++ src/mars-rovers/index.ts
@@ -2,7 +2,7 @@ type Rover = {
   x: number;
   y: number;
   direction: string;
-  command?: string;
+  command?: string[];
 };
 
 const plateauSize = (input: string[]) => {
@@ -33,8 +33,66 @@ const getCommand = (input: string[], rovers: Rover[]) => {
   for (let i = 2, j = 0; i < input.length; i += 2, j++) {
     const command = input[i];
     if (rovers[j]) {
-      rovers[j].command = command;
+      rovers[j].command = command.split("");
     }
+  }
+};
+
+const moveRover = (plateau: { x: number; y: number }, rovers: Rover[]) => {
+  const plateauLimits = plateau;
+
+  rovers.forEach((rover) => {
+    const command = rover.command;
+    command?.forEach((instruction) => {
+      switch (instruction) {
+        case "R":
+          moveR(rover);
+          break;
+        case "L":
+          moveL(rover);
+          break;
+        default:
+          //moveM(plateauLimits, rover);
+          break;
+      }
+    });
+  });
+
+  console.log(rovers); // preciso retornar um array ou obj com as posições finais dos rovers
+  
+};
+
+const moveR = (rover: Rover) => {
+  switch (rover.direction) {
+    case "N":
+      rover.direction = "E";
+      break;
+    case "E":
+      rover.direction = "S";
+      break;
+    case "S":
+      rover.direction = "W";
+      break;
+    case "W":
+      rover.direction = "N";
+      break;
+  }
+};
+
+const moveL = (rover: Rover) => {
+  switch (rover.direction) {
+    case "N":
+      rover.direction = "W";
+      break;
+    case "W":
+      rover.direction = "S";
+      break;
+    case "S":
+      rover.direction = "E";
+      break;
+    case "E":
+      rover.direction = "N";
+      break;
   }
 };
 
@@ -42,10 +100,12 @@ const main = (input: string[]) => {
   const plateau = plateauSize(input); // obj com as dimensões do tabuleiro
   const rovers = getRovers(input); // array de obj de type Rover
 
+  // const finalPosition = moveRover(plateau, rovers);
+
   for (let i = 0; i < rovers.length; i++) {
     const { x, y, direction, command } = rovers[i];
     console.log(`Rover ${i + 1}: ${x} ${y} ${direction} ${command}`);
-  }
+  } // posso usar esse loop para chamar a função de movimentação dos rovers e talvez encapsular isso em uma função e remover a lógica da main.
 
   console.log(`The size of the plateau: ${plateau.x} x ${plateau.y}.`);
 };
